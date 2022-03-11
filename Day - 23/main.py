66a56864ee4071c58b75b3760c69b3bf9b248800
Harsh Raj <harshraj2717@gmail.com>
2022-03-11 20:06:50 +0530

    day - 23 _ starting files

---
+++ Day - 23/main.py
@@ -0,0 +1,20 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+
+def main():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.tracer(0)
+
+    game_is_on = True
+    while game_is_on:
+        time.sleep(0.1)
+        screen.update()
+
+
+if __name__ == "__main__":
+    main()
