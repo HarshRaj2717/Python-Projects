819bd6997536b3c5ac3242d86ecf8066d530475c
harshraj2717 <harshraj2717@gmail.com>
2022-07-15 18:42:32 +0530

    Day - 33

---
+++ Day - 33/check_iss_overhead.py
@@ -0,0 +1,36 @@
+import requests
+
+try:
+    # Getting ISS data.
+    iss_response = requests.get("http://api.open-notify.org/iss-now.json")
+    iss_response.raise_for_status()
+
+    iss_data = iss_response.json()
+
+    # Printing live location of ISS in langitudes and longitudes.
+    print(f'''Location of ISS :
+        Longitude: {iss_data["iss_position"]["longitude"]}
+        Latitude: {iss_data["iss_position"]["latitude"]}''')
+    
+    # Getting live location(from IP) data.
+    abstractapi_response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=f0723f31952b44e1a9d3e69614654434")
+    abstractapi_response.raise_for_status()
+
+    abstractapi_data = abstractapi_response.json()
+
+    # Printing live location of ISS in langitudes and longitudes.
+    print(f'''Your location :
+        Longitude: {abstractapi_data["longitude"]}
+        Latitude: {abstractapi_data["latitude"]}''')
+
+    # Check if ISS overhead.
+    print("Is ISS overhead?", end="\n        ")
+
+    if abstractapi_data["longitude"]-5 < float(iss_data["iss_position"]["longitude"]) < abstractapi_data["longitude"]+5 \
+        and abstractapi_data["latitude"]-5 < float(iss_data["iss_position"]["latitude"]) < abstractapi_data["latitude"]+5:
+        print("YES")
+    else:
+        print("NO")
+
+except Exception as e:
+    print("Error:", e)
