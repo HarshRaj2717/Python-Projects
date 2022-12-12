63065d166b735693dccedf19a78c259e9eeac039
harshraj2717 <harshraj2717@gmail.com>
2022-12-12 19:59:23 +0530

    minor location fixes

--- 13. Cookie Clicker bot - selenium web driver/selenium basics.py
+++ 13. Cookie Clicker bot - selenium web driver/selenium basics.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-chrome_driver_path = "resources/chromedriver_linux64/chromedriver copy"
+chrome_driver_path = "resources/chromedriver_linux64/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("https://en.wikipedia.org/wiki/Main_Page") # Open a site
